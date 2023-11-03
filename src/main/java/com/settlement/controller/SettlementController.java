ad308add312311e5aa365682d17d0f1c832ecc76
anand <anand.bhunia18@gmail.com>
2023-11-03 13:24:51 +0530

    added eureka client and swagger

--- src/main/java/com/settlement/controller/SettlementController.java
+++ src/main/java/com/settlement/controller/SettlementController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.settlement.model.Settlement;
 import com.settlement.service.SettlementService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class SettlementController {
 	// injectservice
@@ -21,6 +25,10 @@ public class SettlementController {
 	private SettlementService settlementService;
 
 	@PostMapping("/addSettlementsDetails")
+	@ApiOperation(value = "Request to add settlement details")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created"), 
+			@ApiResponse(code = 400, message = "Invalid Request"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<Settlement> saveSettlements(@RequestBody Settlement settlement) {
 		Settlement settlement1 = settlementService.saveSettlements(settlement);
 		return ResponseEntity.ok().body(settlement1);
@@ -28,23 +36,36 @@ public class SettlementController {
 	}
 
 	@PutMapping("/updateDetails")
+	@ApiOperation(value = "Request to edit settlement details")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<Settlement> updateSettlements(@RequestBody Settlement settlement) {
 		Settlement settlement1 = settlementService.updateSettlements(settlement);
 		return ResponseEntity.ok().body(settlement1);
 
 	}
-@GetMapping("/getSettlementById/{id}")
+
+	@GetMapping("/getSettlementById/{id}")
+	@ApiOperation(value = "Request to get settlement details using id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
 	public ResponseEntity<Settlement> getSettlements(@PathVariable("id") Integer id) {
 		Settlement settlement = settlementService.getSettlements(id);
 		return ResponseEntity.ok().body(settlement);
 
 	}
-@DeleteMapping("/deleteSettlement/{id}")
-public String deleteSettlement(@PathVariable("id")Integer id) {
-	settlementService.deleteSettlement(id);
-	return"deleteRecord";
-	
-}
 
+	@DeleteMapping("/deleteSettlement/{id}")
+	@ApiOperation(value = "Request to delete settlement details using id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public String deleteSettlement(@PathVariable("id") Integer id) {
+		settlementService.deleteSettlement(id);
+		return "deleteRecord";
+
+	}
 
 }
